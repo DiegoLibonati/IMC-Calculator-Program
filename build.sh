20895be6949b2e052113c67a8c0b3fdc449497c6
Die <dlibonati@leafnoise.io>
2025-09-25 01:12:06 -0300

    fix: fix builds

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 set -e
 
 echo ">>> Activating virtual environment"
-source venv/bin/activate
+
+if [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+elif [ -f "venv/Scripts/activate" ]; then
+    source venv/Scripts/activate
+else
+    echo "No se encontró el entorno virtual. Crealo con: python -m venv venv"
+    exit 1
+fi
 
 echo ">>> Installing build dependencies"
 pip install -r requirements.build.txt
